83d54e5955355b495e4457ebde9e677749e825ba
Shane Whithead <swhitehead@gmail.com>
2016-07-24 07:44:19 +1000

    Add in Fuzi and XML support

--- CioffiServer/RequestHandlerRegistry.swift
+++ CioffiServer/RequestHandlerRegistry.swift
@@ -68,6 +68,7 @@ class DefaultRequestHandler: RequestHandler {
 	func loadFunction(from: URL) throws {
 		let data = try Data(contentsOf: from)
 		let json = JSON(data: data)
+		log(info: "Load from \(from)")
 		guard let type = json["request"]["header"]["type"].int else {
 			throw RequestRegistryError.missingReuqestType
 		}
@@ -76,11 +77,11 @@ class DefaultRequestHandler: RequestHandler {
 	}
 	
 	func handle(request: JSON, forResponder responder: Responder) {
-		
+		// Do we have a type
 		guard let typeCode = request["header"]["type"].int else {
 			return
 		}
-		
+		// Look up the script
 		let requestType = RequestType.for(typeCode)
 		log(info: "requestType: \(requestType)")
 		guard let json = registry[requestType] else {
@@ -89,6 +90,30 @@ class DefaultRequestHandler: RequestHandler {
 			return
 		}
 		
+		var response: [String: [String: AnyObject]] = [:]
+		let responseScript = json["response"]
 		
+		response["header"] = [:]
+		response["header"]?["type"] = responseScript["header"]["type"].intValue
+		response["header"]?["version"] = responseScript["header"]["version"].intValue
+		response["header"]?["result"] = responseScript["header"]["result"].intValue
+		
+		responseScript.
+		
+		log(info: "group = \(responseScript["group"].count)")
+		log(info: "group = \(responseScript["group"].array)")
+		
+		for group in responseScript["group"].arrayValue {
+			let groupName = group["name"].stringValue
+			log(info: "groupName = \(groupName)")
+			for value in group[groupName].arrayValue {
+				let valueName = value["name"].stringValue
+				let valueType = value["type"].stringValue
+				let valueValue = value["value"].stringValue
+				log(info: "valueName = \(valueName)")
+				log(info: "valueType = \(valueType)")
+				log(info: "valueValue = \(valueValue)")
+			}
+		}
 	}
 }
